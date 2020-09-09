da9e4b05755ef9c43eb4f4cd5cf3a98787005f49
Luis Ramirez Mendoza <luism.ramirez23@gmail.com>
2020-09-09 14:41:46 -0700

    finished the first program

---
+++ scrabble_point_calculator.py
@@ -0,0 +1,16 @@
+my_dict = {}
+with open('point_system', 'r') as f:
+	#THIS IS A BLOCK
+	for line in f:
+		#print(char):
+		letter = line.rstrip().lower()
+		value = int (f.readline().rstrip())
+		my_dict[letter] = value
+
+print (my_dict)
+
+word = input("Enter a word\n").lower()
+total_points = 0
+for i in word:
+	total_points += my_dict[i]
+print(total_points)
--- word_games.py
+++ word_games.py
@@ -3,7 +3,9 @@ def main():
 	with open('names', 'r') as f:
 		#THIS IS A BLOCK
 		for line in f:
+			#print(char):
 			print(line)
+			print(f.readline() + "end")
 	#eEVERYTHING PAST HERE, THE FILE IS CLOSED
 if __name__ == "__main__":
 	main()
