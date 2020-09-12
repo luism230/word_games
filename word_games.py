77c3cfa2f20dca4089b57b0855d53f2dc58a694b
Luis Ramirez Mendoza <luism.ramirez23@gmail.com>
2020-09-11 19:09:19 -0700

    idk

--- word_games.py
+++ word_games.py
@@ -9,3 +9,9 @@ def main():
 	#eEVERYTHING PAST HERE, THE FILE IS CLOSED
 if __name__ == "__main__":
 	main()
+
+	def main():
+	with open('names', 'r') as f:
+		#THIS IS A BLOCK
+		for line in f:
+			print(line)
